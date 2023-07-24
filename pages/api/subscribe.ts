f99311200421a978148e3e26de4b972b1f68ddc2
geepy technologies <geepytechnologies@gmail.com>
2023-07-24 18:54:50 +0100

    new

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -2,6 +2,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import User from "../../models/User";
 import dbConnect from "../../utils/db";
 import axios from "axios";
+import nodemailer from "nodemailer";
+import fs from "fs";
+import path from "path";
 
 type Data = {
   name: string;
@@ -13,10 +16,39 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { method } = req;
+  const { firstname, lastname, email, phone } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.zoho.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "admin@spiritfilledwomen.org",
+      pass: "spiritfilledwomen2023",
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+  //read email template
+  const filePath = path.join(process.cwd(), "public", "test.html");
+  const fileContent = await fs.promises.readFile(filePath, "utf-8");
+  // Replace [Username] with actual username
+  const username = "John Doe";
+  const personalizedEmail = fileContent
+    .replace("[username]", firstname)
+    .replace("[email]", email);
+
+  // Email message options
+  const mailOptions = {
+    from: "admin@spiritfilledwomen.org",
+    to: email,
+    subject: "Spiritfilledwomen",
+    html: personalizedEmail,
+  };
 
   await dbConnect();
   if (method === "POST") {
-    const { firstname, lastname, email, phone } = req.body;
     const alreadySubscribed = await User.findOne({ email: email });
     try {
       if (alreadySubscribed) {
@@ -28,11 +60,9 @@ export default async function handler(
           ...req.body,
         });
         const userRes = await user.save();
-        const emailres = await axios.post("http://localhost:3000/api/email", {
-          firstname,
-          email,
-        });
-        res.status(201).json({ email: emailres.data, user: userRes.data });
+        const emailres = await transporter.sendMail(mailOptions);
+
+        res.status(201).json({ email: emailres, user: userRes.data });
       }
     } catch (error: any) {
       return res.status(error.status || 500).end(error.message);
